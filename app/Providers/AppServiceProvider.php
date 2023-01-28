3dab0b2b429341d449e9abe2e8245c813027fbff
Clay <thejourney318@gmail.com>
2023-01-28 15:07:44 -0500

    Edited AppServiceProvider to try to fix 'specified key too long' err.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +27,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         URL::forceScheme('https');
+        Schema::defaultStringLength(191);
     }
 }
