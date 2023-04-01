e1da3671e72b08c0bc8c6d2378b1e0463cf681e2
Clay <thejourney318@gmail.com>
2023-04-01 17:48:49 -0400

    ..

--- resources/js/axiosConfig.js
+++ resources/js/axiosConfig.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-// axios.defaults.headers.common['X-CSRF-TOKEN'] = document.head.querySelector('meta[name="csrf-token"]').content;
 axios.defaults.withCredentials = true;
 
 export default axios;
