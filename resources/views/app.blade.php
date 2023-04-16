84b070466b26c193376cc717a40d8972740231b5
Clay <thejourney318@gmail.com>
2023-04-16 17:00:42 -0400

    Changed font.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,10 @@
         <title inertia>{{ config('app.name', 'Laravelqqq') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+        {{-- <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap"> --}}
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600;700;800&family=Roboto:wght@400;700&family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
