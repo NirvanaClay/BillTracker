9f73bc653f02d97bbc43b530f39e8459ce87d144
Clay <thejourney318@gmail.com>
2024-06-20 00:54:47 -0400

    Slight improvement to file structure.

---
+++ resources/js/Pages/RegisterForm.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, redirect, useNavigate } from 'react-router-dom';
+
+import axios from '../axiosConfig';
+
+const RegisterForm = ({ email, setEmail, password, setPassword, password_confirmation, setPasswordConfirmation, setLoginStatus, totalExpenses, setTotalExpenses, setUserExpenses, setGuestExpenses }) => {
+
+  const navigate = useNavigate()
+
+  const [registerErrors, setRegisterErrors] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post('register', {email, password, password_confirmation})
+    .then((response) => {
+      setLoginStatus(true)
+      setUserExpenses([]); 
+      setGuestExpenses([]);
+      setTotalExpenses(0)
+      navigate('/', { replace: true })
+    })
+    .catch((e) => {
+      console.error(e);
+      setRegisterErrors(Object.values(e.response.data.errors).flat());
+    })
+  }
+
+  return (
+    <div className='register-container'>
+      <form className='register-form' onSubmit={handleSubmit}>
+        <div className='register-field'>
+          <label htmlFor='email'>
+            Email:
+          </label>
+            <input 
+              type="email" 
+              name='email'
+              value={email} 
+              onChange={e => setEmail(e.target.value)} 
+              required 
+            />
+        </div>
+        <div className='register-field'>
+          <label htmlFor='password'>Password:</label>
+            <input 
+              type="password" 
+              name='password'
+              value={password} 
+              onChange={e => setPassword(e.target.value)} 
+              required 
+            />
+        </div>
+        <div className='register-field'>
+          <label htmlFor='password_confirmation'>
+            Confirm Password:
+          </label>
+            <input 
+              type="password" 
+              name='password_confirmation'
+              value={password_confirmation} 
+              onChange={e => setPasswordConfirmation(e.target.value)} 
+              required 
+            />
+        </div>
+        {registerErrors.length > 0 && (
+          <ul>
+            {registerErrors.map((message, index) => (
+              <li key={index} className='register-error'>*{message}</li>
+            ))}
+          </ul>
+        )}
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
+
+export {RegisterForm};
